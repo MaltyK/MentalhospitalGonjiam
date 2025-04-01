f52c149f5975bfc57780a8d5bc4ef9c72212de48
kolla228 <kolya18432590@gmail.com>
2025-04-01 10:57:09 +0700

    nasral

--- hospital/hospital_app/serializers.py
+++ hospital/hospital_app/serializers.py
@@ -1,7 +1,27 @@
 from rest_framework import serializers
-from .models import Item
+from .models import Patient, Doctor, Service, Appointment, Diagnose
 
-class ItemSerializer(serializers.ModelSerializer):
+class PatientSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Item
+        model = Patient
+        fields = '__all__'
+
+class DoctorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Doctor
+        fields = '__all__'
+
+class ServiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Service
+        fields = '__all__'
+
+class AppointmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Appointment
+        fields = '__all__'
+
+class DiagnoseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Diagnose
         fields = '__all__'
