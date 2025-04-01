7e340b46595b22009e5e6f72c0b58d215b9abe5b
dmiitryp <dianasnipova@gmail.com>
2025-04-01 09:44:26 +0700

    upd

--- python/postgre-sql-db.py
+++ python/postgre-sql-db.py
@@ -1,28 +1,60 @@
 from sqlalchemy import create_engine # движок для подключения
 from sqlalchemy.orm import DeclarativeBase # базовая модель
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker # сессия
 from sqlalchemy import  Column, Integer, String, DateTime, CHAR, Date, Boolean
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+# from django.conf import settings
 
-# подключение движка
-# engine = create_engine("postgresql://postgre:qwerty1234@localhost/mydb")
+# # Получаем параметры подключения из settings.py
+# db_name = settings.DATABASES['default']['NAME']
+# db_user = settings.DATABASES['default']['USER']
+# db_password = settings.DATABASES['default']['PASSWORD']
+# db_host = settings.DATABASES['default']['HOST']
+# db_port = settings.DATABASES['default']['PORT']
+
+# # Создаем строку подключения для SQLAlchemy
+# connection_string = f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
+
+# Строка подключения
+connection_string = "postgresql://postgre:qwerty1234@localhost/mydb"
+# Создаем движок SQLAlchemy
+engine = create_engine(connection_string)
 
 # создание класса с базовой моделью
-class Base(DeclarativeBase): pass
+Base = declarative_base()
 
-# Пациенты: id, ФИО, дата рождения, пол, телефон, email, адрес, полис ОМС
+# Записи на приём
+class Appointment(Base):
+    __tablename__ = "appointments"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    patient_id = Column(Integer, ForeignKey('patients.id'), nullable=False)
+    doctor_id = Column(Integer, ForeignKey('doctors.id'), nullable=False)
+    service_id = Column(Integer, ForeignKey('services.id'), nullable=False)
+    datetime = Column(DateTime, nullable=False)
+    status = Column(String, nullable=False)
+
+    patient = relationship("Patient", back_populates="appointments")
+    doctor = relationship("Doctor", back_populates="appointments")
+    service = relationship("Service", back_populates="appointments")
+    diagnoses = relationship("Diagnose", back_populates="appointment")
+
 class Patient(Base):
     __tablename__ = "patients"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    fio = name = Column(String)
+    fio = Column(String)
     birthdate = Column(Date)
-    gender = Column(String) # бывший Column(CHAR)
+    gender = Column(String)
     phone = Column(String)
     email = Column(String)
     adress = Column(String)
     oms = Column(String)
 
-# Врачи: id, ФИО, специальность, кабинет, номер телефона, email.
+    appointments = relationship("Appointment", back_populates="patient")
+
 class Doctor(Base):
     __tablename__ = "doctors"
 
@@ -33,7 +65,9 @@ class Doctor(Base):
     phone = Column(String)
     email = Column(String)
 
-# Услуги: id, наименование, стоимость, описание, длительность
+    appointments = relationship("Appointment", back_populates="doctor")
+    timetables = relationship("Timetable", back_populates="doctor")
+
 class Service(Base):
     __tablename__ = "services"
 
@@ -43,40 +77,9 @@ class Service(Base):
     description = Column(String)
     duration = Column(Integer)
 
-# Записи на приём: id, пациент, врач, дата и время, услуга, статус (запланировано/выполнено/отменено).
-class Appointment(Base):
-    __tablename__ = "appointments"
+    appointments = relationship("Appointment", back_populates="service")
 
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    # [пациент из patients]
-    # [врач из doctors]
-    datetime = Column(DateTime)
-    # [услуга из services]
-    status = Column(String)
-
-# Диагнозы: id, приём, диагноз, комментарии врача.
 class Diagnose(Base):
     __tablename__ = "diagnoses"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    # [id от appointments]
-    diagnose = Column(String)
-    comment = Column(String)
-
-# Расписание врачей: id, врач, день недели, рабочее время (с/до), доступность.
-class Timetable(Base):
-    __tablename__ = "timetables"
-
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    # [врач из doctors]
-    day = Column(String)
-    worktime = Column(String)
-    availability = Column(Boolean)
-
-
-# создаем таблицы
-Base.metadata.create_all(bind=engine)
-print("База данных и таблица созданы")
-
-# создаем класс сессии
-Session = sessionmaker(autoflush=False, bind=engine)
