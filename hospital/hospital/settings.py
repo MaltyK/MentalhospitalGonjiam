0f791501d515c97f0733e49290d076729292dd90
kolla228 <kolya18432590@gmail.com>
2025-04-09 12:46:34 +0700

    added registration, logining (частично)

--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'hospital_app',
 ]
 
@@ -130,3 +131,10 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Rest_framework
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
