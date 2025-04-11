fd9997c99c241fd80999df38f3380d1696acc0f8
kolla228 <kolya18432590@gmail.com>
2025-04-11 12:26:09 +0700

    fully working clientside build

--- hospital/hospital_app/urls.py
+++ hospital/hospital_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/registr/', views.registr, name='registr'),
-    path('accounts/profile/', views.home, name='profile'),
+    path('accounts/profile/', views.profile, name='profile'),
+    path('accounts/appointments/add_appointment/', views.add_appointment, name='appointment'),
+    path('accounts/doctors/doctor_list/', views.doctor_list, name='doctors')
 ]
